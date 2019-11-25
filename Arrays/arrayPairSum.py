d4682e8e2597d7206496b859f91e2d1a9d987d9e
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-25 22:11:36 +0100

    Modified the thePairs function to a one-liner

--- Arrays/arrayPairSum.py
+++ Arrays/arrayPairSum.py
@@ -17,15 +17,21 @@ def pairSum(arr,n, sum):
 				count += 1
 	return count
 	
-def thePairs(arr,n, sum): 
-	count = 0
-	for i in range(0,n):
-		for j in range(i+1,n):
-			if arr[i] + arr[j] == sum:
-				count +=1
-				# print([   (arr[i],arr[j]) for i,j  in arr  ])
-				print ([(arr[i], arr[j])])
-	return 0
+# def thePairs(arr,n, sum): 
+# 	count = 0
+# 	for i in range(0,n):
+# 		for j in range(i+1,n):
+# 			if arr[i] + arr[j] == sum:
+# 		 		count +=1
+# 		 		print ([(arr[i], arr[j])])
+		        
+# 	return ("The number of pairs is: ",count)
+
+
+def thePairs(arr,n, sum):
+	diff_pairs = [(arr[i], sum-arr[i]) for i in range(0, n) if sum-arr[i] in arr]
+
+	return ("The pairs are: ", diff_pairs) 
 
 
 
