afba628aa4311318781c3f25973de1ae0460199e
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-28 20:44:00 +0100

    Not fully grasping this yet

--- Arrays/2DArray.py
+++ Arrays/2DArray.py
@@ -7,9 +7,10 @@ arr: an array of integers
 '''
 
 
-rows, cols = (6, 6) 
+row = 6
+col = 6
 
-arr = [[0 for i in range(cols)] for j in range(rows)] 
+arr = [[0 for i in range(col)] for j in range(row)] 
   
 # again in this new array lets change 
 # the first element of the first row  
@@ -32,10 +33,39 @@ arr [4][3] = 2
 
 arr[5][2] = 1
 arr[5][3] = 2
-arr[5][4] = 4  
+arr[5][4] = 4
+
+
+# def hourglassSum(arr):
+
+# # start max_hourglass_sum at smallest possible hourglass
+# 	max_hourglass_sum = -63  
+
+# 	def __get_hourglass_sum(arr, row, col):
+# 		sum = 0
+# 		sum += matrix[row-1][col-1] # top left
+# 		sum += matrix[row-1][col]  # top center
+# 		sum += matrix[row-1][col+1] # top right
+# 		sum += matrix[row][col] # lone center
+# 		sum += matrix[row+1][col-1] #bottom left
+# 		sum += matrix[row+1][col] # bottom center
+# 		sum += matrix [row+1][col+1] #bottom right
+
+# 		return sum
+
+# 	for i in range(1,5):
+# 	    for j in range(1, 5):
+# 	        current_hourglass_sum = _get_hourglass_sum(arr, row, col)
+# 	        if current_hourglass_sum > max_hourglass_sum:
+# 	            max_hourglass_sum = current_hourglass_sum
+
+# 	print(max_hourglass_sum)
+
+
+
+# hourglassSum(arr)
 
 
 
 
-for row in arr: 
-    print(row) 
+
