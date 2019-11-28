526d6b3a91c97258667f5f7681bdb575b493f796
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-28 20:43:05 +0100

    Optiimized the code for the uniqueness of the pairs

--- Arrays/arrayPairSum.py
+++ Arrays/arrayPairSum.py
@@ -8,37 +8,19 @@
 
 #  (1,3)
 #  (2,2)
-
-def pairSum(arr,n, sum): 
-	count = 0 #Initializing the count of the number of pairs
-	for i in range(0,n):
-		for j in range(i+1,n):
-			if arr[i] + arr[j] == sum:
-				count += 1
-	return count
-	
-def thePairs(arr,n, sum): 
-	empty_list = []
-	for i in range(0,n):
-		for j in range(i+1,n):
-			if arr[i] + arr[j] == sum:
-		 		empty_list.append((arr[i], arr[j]))
-		        
-	return ("The number of pairs is: ",empty_list)
-
-
-# def thePairs(arr,n, sum):
 	
 
-# 	diff_pairs = [(arr[i], sum-arr[i]) for i in range(0, n) if sum-arr[i] in arr]
-# 	diff_pairs = list(dict.fromkeys(diff_pairs))
-	
-
-# 	return diff_pairs
-
-# There's a problem with this sha, it is not accurate. Need to look into tweaking this
-
-
+def uniqueTwoSum(arr, sum):
+  ans, comp = set(), set()
+  for n in arr:
+    c = sum-n
+    if c in comp:
+      res = (n, c) if n > c else (c, n)
+      if res not in ans:
+        ans.add(res)
+    comp.add(n)
+  print(ans)
+  return len(ans)
 
 
 
@@ -48,6 +30,7 @@ arr = [1,5,7,-1,5]
 n = len(arr)
 sum = 6
 
-print("Count of pairs is: ", pairSum(arr,n,sum))
 
-print (thePairs(arr,n, sum))
+print(uniqueTwoSum(arr,sum))
+
+print()
