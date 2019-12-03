630c4a9c3c53a90abe2f351e31f0619101d7d26d
AbdussamadYisau <Sammieyisau@gmail.com>
2019-12-03 01:00:17 +0100

    Solved the question finding all contigous subarrays within a bigger array that sum up to zero

---
+++ Miscellanous/subarraysToZero.py
@@ -0,0 +1,135 @@
+# def printAllSubarrays(arr,n):
+# 	#Consider all sub-arrays starting from i
+# 	i = 0
+# 	for i in range(n):
+# 		sum = 0 #tracker for sum of elements
+# 		#consider all sub arrays ending at j
+# 		j = i
+
+# 		for j in range(n):
+# 			#Sum of elements so far
+
+# 			sum += arr[j]
+
+# 			if(sum == 0):
+# 				print("Subarray [{i}..{j}]".format(i = i, j =j))
+
+
+# arr = [3,4,-7,3,1,3,1,-4,-2,-2]
+# n = len(arr)
+
+# printAllSubarrays(arr,n)
+
+
+
+
+def printAllSubarrays(arr, n):
+
+    hashMap = {}
+
+    #Create an array to hold the sub arrays
+
+    hold = []
+
+    #tracker for sum of elements
+
+    sum = 0
+
+    for i in range(n):
+        sum += arr[i]
+
+        #If sum is 0, we have successfully found a subarray starting from index 0 and ending at index I
+
+        if sum == 0:
+            hold.append((0,i))
+
+        alreadyExists = []
+        
+        #Check if sum exists in hashmap already
+        if sum in hashMap: 
+            alreadyExists = hashMap.get(sum)
+            alreadyExistLength = len(alreadyExists)
+            for ele in range(alreadyExistLength):
+                hold.append((alreadyExists[ele] + 1, i))
+        alreadyExists.append(i)
+        hashMap[sum] = alreadyExists
+    
+    print(hold)
+
+
+arr = [3,4,-7,3,1,3,1,-4,-2,-2]
+n = len(arr)
+
+printAllSubarrays(arr,n)
+
+#Trying out hashmap now
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
