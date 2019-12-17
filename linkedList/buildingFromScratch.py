dc924dab6cfb892e7cfd62c208a412cda6489491
AbdussamadYisau <Sammieyisau@gmail.com>
2019-12-17 15:42:56 +0100

    Building a singly linked list from scratch

---
+++ linkedList/buildingFromScratch.py
@@ -0,0 +1,23 @@
+# This is the implementation of a singly linked  list
+
+class Node:
+	def __init__(self,value):
+		self.info = value # This refers to the actual data in the node
+		self.link = None # This refers to the "link"
+
+class singleLinkedList:
+	def __init__(self):
+		self.start = None
+	def displayList(self):
+		pass
+	def countNodes(self):
+		pass
+	def search(self,x):
+		pass
+	def insertInBeginning(self,data):
+		pass
+	def insertAtEnd(self,data):
+		pass
+	def createList(self):
+		pass
+	
