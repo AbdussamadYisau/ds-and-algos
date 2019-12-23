2a09a9bd8abbe40ac55938cbc0d9593d81f3c102
AbdussamadYisau <Sammieyisau@gmail.com>
2019-12-23 07:07:23 +0100

    linked list implementation and most of its method

---
+++ linkedList/linkedListAndMostMethods.py
@@ -0,0 +1,65 @@
+# This is the implementation of a singly linked  list
+
+class Node:
+	def __init__(self,value):
+		self.info = value # This refers to the actual data in the node
+		self.link = None # This refers to the "link"
+
+class singleLinkedList:
+	def __init__(self):
+		self.start = None
+	def displayList(self):
+		pass
+	def countNodes(self):
+		pass
+	def search(self,x):
+		pass
+	def insertInBeginning(self,data):
+		pass
+	def insertAtEnd(self,data):
+		pass
+	def createList(self):
+		pass
+	def insertBefore(self,data,x):
+		pass
+	def insertAtPosition(self,data,k):
+		pass
+	def deleteNode(self,x):
+		pass
+	def deleteFirstNode(self):
+		pass
+	def deleteLasttNode(self):
+		pass
+	def reverseList(self):
+		pass
+	def bubbleSortExdata(self): # By exchanging data
+		pass
+	def bubbleSortExlinks(self): # By exchanging links
+		pass
+	def hasCycle(self):
+		pass
+	def findCycle(self):
+		pass
+	def removeCycle(self):
+		pass
+	def insertCycle(self):
+		pass
+	def merge2(self,list2):
+		pass
+	def _merge2(self,p1, p2):
+		pass
+	def mergeSort(self):
+		pass
+	def _mergeSortRec(self, listStart):
+		pass
+	def _divideList(self, p):
+		pass
+
+
+
+
+
+
+
+
+ 
