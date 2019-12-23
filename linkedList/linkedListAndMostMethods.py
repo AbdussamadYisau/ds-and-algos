50cfeb10529d36202fef28154642206b6196f98e
AbdussamadYisau <Sammieyisau@gmail.com>
2019-12-23 09:05:38 +0100

    Wrote the code for three methods: display list, count nodes and search list

--- linkedList/linkedListAndMostMethods.py
+++ linkedList/linkedListAndMostMethods.py
@@ -9,11 +9,37 @@ class singleLinkedList:
 	def __init__(self):
 		self.start = None
 	def displayList(self):
-		pass
+		if self.start is None:
+			print("List is empty")
+			return
+		else:
+			print("List is:    ")
+			p = self.start
+			while p is not None:
+				print(p.info, sep = " ", end = ",")
+				p = p.link
+			print()
 	def countNodes(self):
-		pass
+		p = self.start
+		n = 0
+
+		while p is not None:
+			n+= 1
+			p = p.link
+		print("Number of nodes in the list: ", n)
 	def search(self,x):
-		pass
+		position = 1 # Initializing position of the default position 
+		p = self.start
+		while p is not None:
+			if p.info == x:
+				print(x, " is at position", position)
+				return True
+			position += 1
+			p = p.link
+		else:
+			print(x, " not found in list." )
+			return False 
+
 	def insertInBeginning(self,data):
 		pass
 	def insertAtEnd(self,data):
@@ -55,6 +81,41 @@ class singleLinkedList:
 	def _divideList(self, p):
 		pass
 
+
+list = singleLinkedList()
+
+list.createList()
+
+while True: 
+	print("1. Display List.")
+	print("2. Count the number of nodes.")
+	print("3. Search for an element.")
+	print("4. Insert in empty list/ insert in beginning of the list.")
+	print("5. Insert a node at the end of the list.")
+	print("6. Insert a node after a specified node.")
+	print("7. Insert a node before a specified node.")
+	print("8. Insert a node at a given position")
+	print("9. Delete the first node.")
+	print("10. Delete last node.")
+	print("11. Delete any node.")
+	print("12. Reverse the list.")
+	print("13. Bubble sort by exchanging data.")
+	print("14. Bubble sort by exchanging links.")
+	print("15. Merge sort.")
+	print("16. Insert cycle.")
+	print("17. Delete cycle.")
+	print("18. Remove cycle.")
+	print("19. Quit.")
+
+	option = int(input("Enter your choice:  "))
+
+	if option == 1:
+		list.displayList()
+	elif option == 2:
+		list.countNodes()
+	elif option == 3:
+		data = int(input("Enter the element that you want to search for: "))
+		list.search(data)
 
 
 
