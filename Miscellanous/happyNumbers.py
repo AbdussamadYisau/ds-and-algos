90cfa268ed4526b276a983f1c052f538b8e28e3b
AbdussamadYisau <Sammieyisau@gmail.com>
2020-04-02 10:56:01 +0100

    Wrote code to determine whether a number is a happy Number or not. Added docstring

--- Miscellanous/happyNumbers.py
+++ Miscellanous/happyNumbers.py
@@ -3,6 +3,17 @@ def squareDigitSum(n):
 
 
 def happyNumber(n):
+	"""
+	A happy number is a number defined by the following process: 
+	Starting with any positive integer, replace the number by the sum of the squares of its digits,
+	 and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+	Those numbers for which this process ends in 1 are happy numbers.
+
+	Input: 19
+	Output: True
+
+
+	"""
 
 	while True:
 
