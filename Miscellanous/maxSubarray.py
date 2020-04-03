f21b591df8708a3c43c3efa070f9acc08b0271c3
AbdussamadYisau <Sammieyisau@gmail.com>
2020-04-03 22:12:26 +0100

    Kadane's algorithmic implementation of Maximum contiguous subarray problem

---
+++ Miscellanous/maxSubarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums):
+        max_seq = nums[0]
+        curr_sum = nums[0]
+        for num in nums[1:]:
+            if curr_sum < 0:
+                curr_sum = num
+            else:
+                curr_sum += num
+            if curr_sum > max_seq:
+                max_seq = curr_sum
+        return max_seq
+            
+            
+example = Solution()
+
+
+                
+        
+nums = [-2,1,-3,4,-1,2,1,-5,4]          
+
+print(example.maxSubArray(nums))
