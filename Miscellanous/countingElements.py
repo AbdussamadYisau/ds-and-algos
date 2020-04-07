03ff79827ca5511743bdffd69143dab63cccbe8b
Yisau Abdussamad <sammieyisau@gmail.com>
2020-04-07 13:46:23 +0100

    Wrote code for LeetCode's Counting Elements

---
+++ Miscellanous/countingElements.py
@@ -0,0 +1,15 @@
+def countElements(arr):
+    count = {}
+    for i in arr:
+        count[i] = count.get(i,0) + 1
+    total = 0
+
+    for k,v in count.items():
+        if k + 1 in count:
+            total += v
+    print(count.items())
+    return total
+
+arr = [1,2,3]
+
+print(countElements(arr))
