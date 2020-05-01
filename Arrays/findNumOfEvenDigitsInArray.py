5817127d29b262276ea7f7760a3da25173bd0fd4
Yisau Abdussamad <sammieyisau@gmail.com>
2020-05-01 18:11:49 +0100

    Added a secondway to solve the same question, a more pythonic way if you will

--- Arrays/findNumOfEvenDigitsInArray.py
+++ Arrays/findNumOfEvenDigitsInArray.py
@@ -19,4 +19,17 @@ class Solution:
                 ans += 1
         return ans
             
+class SolutionOne:
+    def findNumbers(self, nums: List[int]) -> int:
+        ans = 0
         
+        
+        for num in nums:
+            count = 0
+            while num != 0:
+                num //= 10 
+                count += 1
+            if count % 2 == 0:
+                ans += 1
+                
+        return ans
