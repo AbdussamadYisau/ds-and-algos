c92c9df5395237853ba6945c1afc0b6d1ee483b8
Yisau Abdussamad <sammieyisau@gmail.com>
2020-05-05 12:21:50 +0100

    Found another way to go about the max consecutive ones question using array

--- Arrays/maxConsecutiveOnes.py
+++ Arrays/maxConsecutiveOnes.py
@@ -13,3 +13,20 @@ class Solution:
             else:
                 count = 0
         return maxOnes
+# Another method 
+def num_of_ones(list_value):
+    count = 0
+    new = []
+    for i in list_value:
+        if i == 1:
+            count += 1
+            new.append(count)  
+        elif i == 0:
+            count = 0
+    return max(new)
+
+          
+l1 = [1,1,0,1,1]
+l2 = [1,1,1,0,0,0,0,1]
+ans =num_of_ones(l2)
+print(ans)
