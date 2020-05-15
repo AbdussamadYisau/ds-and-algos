b7411f9d74d358967b4690bb6333d45ed1344bcf
AbdussamadYisau <Sammieyisau@gmail.com>
2020-05-15 01:19:10 +0100

    Finally understand the idea of two pointer approach.

---
+++ Arrays/removeDupLicatesFromArray.py
@@ -0,0 +1,29 @@
+'''
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+
+'''
+
+class Solution:
+    def removeDuplicates(self, nums):
+        
+        # array size
+        n = len(nums)
+        # i is the slower runner in the 2-pointer approach
+        i = 0
+        # j is the fast runner in the 2-pointer approach
+        j = 1
+        
+        while (j<n):
+            if (nums[i] != nums[j]):
+                i += 1
+            nums[i] = nums[j]
+            j += 1
+            
+        return(i+1)
+        
