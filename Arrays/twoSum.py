c5235f390560a8314fa07e70be45ba5578795644
AbdussamadYisau <Sammieyisau@gmail.com>
2020-05-16 23:24:08 +0100

    Added an optimized code for the twoSum problem

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -11,6 +11,7 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 '''
 
+# Brute Force Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
@@ -18,4 +19,16 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return(i,j)
+
+# Optimized solution - Hashmap
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dictionary:
+                return(dictionary[complement], i)
+            else:
+                dictionary[nums[i]] = i
+            
         
