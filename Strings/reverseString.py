76f5d51a23a51c3d41c434cb8ebe066c968d147f
AbdussamadYisau <Sammieyisau@gmail.com>
2020-05-28 20:55:25 +0100

    Leetcode's reverse string

---
+++ Strings/reverseString.py
@@ -0,0 +1,20 @@
+# A LeetCode question - Solved with the two pointer approach. It was supposed to be solved in-place
+
+class Solution():
+
+    def reverseString(self, s):
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        
+        while (i < j):
+            s[i], s[j] = s[j], s[i]
+            
+            i += 1
+            j -= 1
+        return s #If this is removed, it would be in-place. I included it so that we can see if it does what it is supposed to.
+
+l = Solution()
+print(l.reverseString(["H","e", "l", "l", "o"]))
