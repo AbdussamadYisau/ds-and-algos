d1f254a6de5dadc466f353fe4cffe0b86f225994
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-26 21:37:13 +0100

    Hackerrank Save The Prisoner naive solution

---
+++ Miscellanous/saveThePrisoner.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+#Naive solution
+
+def saveThePrisoner(n, m, s):
+    count = s
+
+    for i in range(m - 1):
+        if (count >= n):
+            count = 1
+        else:
+            count += 1
+    return count
+
+print(saveThePrisoner(5,2,2));
