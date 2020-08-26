e2a82c3a45bda5973366150e4c1209e0b672bbab
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-26 23:06:03 +0100

    Efficient solution, no dey work yet

--- Arrays/threeSum.py
+++ Arrays/threeSum.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/3sum/
 
+#Naive Solution
 def threeSum(nums):
     res = []
     for i in range(len(nums)):
@@ -12,5 +13,33 @@ def threeSum(nums):
     
     return res
 
+# Efficient Solution
+
+def threeSumEfficient(nums):
+    nums.sort()
+
+    res = []
+
+    print(nums.sort())
+
+    for i in range(len(nums)):
+
+        #Two-pointer method to find the other characters
+
+        secondNum = i + 1
+
+        lastNum = len(nums) - 1
+
+        while (secondNum < lastNum):
+            if(nums[i] + nums[secondNum] + nums[lastNum] == 0):
+                res.append([nums[i], nums[secondNum], nums[lastNum]])
+            elif (nums[i] + nums[secondNum] + nums[lastNum] < 0):
+                secondNum += 1
+            else:
+                lastNum -= 1
+    return res
+
 print(threeSum([-1, 0, 1, 2, -1, -4]))
 print(threeSum([-3, 3 , 3, 3, 3, 3]))
+print("==============================================")
+print(threeSumEfficient([-1, 0, 1, 2, -1, -4]))
