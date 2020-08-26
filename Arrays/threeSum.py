77452806006cfee36ca81a3afab64ab1b966873b
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-26 22:00:19 +0100

    threeSum Naive Solution

---
+++ Arrays/threeSum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/3sum/
+
+def threeSum(nums):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    return(nums[i], nums[j], nums[k])
+
+print(threeSum([-1, -1 , 2, 3, 3, 3]))
