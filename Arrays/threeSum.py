2febab6221956dea415e735838d8d50db7ec9ad6
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-29 00:19:49 +0100

    Adjusted Naive solution to account for only unique triplets

--- Arrays/threeSum.py
+++ Arrays/threeSum.py
@@ -3,7 +3,10 @@
 #Naive Solution
 def threeSum(nums):
     res = []
-    for i in range(len(nums)):
+    nums.sort()
+    for i in range(len(nums) - 2):
+        if (i>0 and nums[i]==nums[i-1]): 
+            continue
         for j in range(i+1, len(nums)):
             for k in range(j+1, len(nums)):
                 if nums[i] + nums[j] + nums[k] == 0:
