eca6ac7ceab85916a78f8878372b68654f9b1424
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-31 22:52:11 +0100

    reverseVowels another way

--- Strings/reverseVowels.py
+++ Strings/reverseVowels.py
@@ -19,13 +19,43 @@ class Solution:
                 i += 1
             elif L[j] not in vowels:
                 j -= 1
+            
+            else: 
+                L[i], L[j] = L[j], L[i]
+                
+                i += 1
+                j -= 1
+       
+        return (''.join(L))
+
+
+    def reverseVowelsAnotherWay(self, s: str) -> str:
+        vowels = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'}
+        
+        L = list(s)
+        
+        i = 0
+        j = len(s) - 1
+        
+        while i<j:
+            
+            while i < j and L[i] not in vowels:
+                i += 1
+            
+            while i < j and L[j] not in vowels:
+                j -= 1
+            
+            
+            
             L[i], L[j] = L[j], L[i]
             
             i += 1
             j -= 1
-       
+        
         return (''.join(L))
         
 test = Solution()
 
 print(test.reverseVowels("leetcode"))
+print("===================================")
+print(test.reverseVowelsAnotherWay("leetcode"))
