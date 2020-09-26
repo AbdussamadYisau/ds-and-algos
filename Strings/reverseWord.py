42c4476ad8e26f8cc462a46a0a52482e029cfb86
AbdussamadYisau <Sammieyisau@gmail.com>
2020-09-26 22:01:17 +0100

    reverse word

---
+++ Strings/reverseWord.py
@@ -0,0 +1,48 @@
+'''
+Given an input string, reverse the string word by word.
+
+ 
+
+Example 1:
+
+Input: "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: "  hello                      world!  "
+sol= []
+"hello        world!"
+Output: "world! hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+'''
+
+def revWord(expr):
+    
+    n = len(expr)
+    start = end = n -1
+    out  = []
+    while start >= 0:
+        if expr[end] == ' ':
+            end -= 1
+             
+        if expr[start] != ' ' and ((start-1) < 0 or expr[start-1] == ' '):
+            out.append(expr[start:end+1])
+            end = start - 1
+        start -= 1
+    return out
+        
+    
+    
+print(revWord("  hello           guy   man  someting foo      bar   z oo         oz la la           world!  "))
+    
+    
+
+        
+    
+        
