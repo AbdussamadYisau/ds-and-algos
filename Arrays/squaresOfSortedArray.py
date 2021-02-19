6e165a82beb73a192fed4527d3d8c3c59aa4631a
AbdussamadYisau <sammieyisau@gmail.com>
2021-02-19 01:15:03 +0100

    Efficient squares of sorted array

--- Arrays/squaresOfSortedArray.py
+++ Arrays/squaresOfSortedArray.py
@@ -1,10 +1,43 @@
+# This fails for large inputs, because remove is O(n). It also fails for courses where there are not unique numbers
 class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        solArray = []
+    def sortedSquares(self, nums):
+        ans = []
         
-        for num in A:
-            solArray.append(num**2)
-        solArray.sort()
+        for i in range(len(nums)):
+            nums[i] = nums[i]**2
+        for i in range(len(nums)):
+            ans.append(min(nums))
+            nums.remove(min(nums))
+        return ans
+
+t = Solution()
+
+print(t.sortedSquares([-4,-1,0,3,10]))
+
+class SolutionOne:
+    def sortedSquares(self, nums):
+        n = len(nums)
+        left, right = 0, n - 1
+        index = n - 1
+        result = [0 for x in nums]
+    
+        while index >= 0:
+    
+            if abs(nums[left]) >= abs(nums[right]):
+                result[index] = nums[left] * nums[left]
+                left += 1
+            else:
+                result[index] = nums[right] * nums[right]
+                right -= 1
+            index -= 1
         
-        return solArray
+        for i in range(n): 
+            nums[i] = result[i]
+
+        return nums
+        
+
+s = SolutionOne()
+
+print(s.sortedSquares([-4,-1,0,3,10]))      
         
